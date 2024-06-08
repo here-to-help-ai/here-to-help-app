6a59c80b50da75f6f62f2f8a977cb22b66c7aa00
s3851781@student.rmit.edu.au <s3851781@student.rmit.edu.au>
2024-06-08 10:06:15 +1000

    rename

--- src/server/api/routers/ai.ts
+++ src/server/api/routers/ai.ts
@@ -2,12 +2,8 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
-let post = {
-  id: 1,
-  name: "Hello World",
-};
 
-export const postRouter = createTRPCRouter({
+export const aiRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -22,11 +18,6 @@ export const postRouter = createTRPCRouter({
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      post = { id: post.id + 1, name: input.name };
-      return post;
     }),
 
-  getLatest: publicProcedure.query(() => {
-    return post;
-  }),
 });
